ab6c1f2ccfa912c99496d6d0f8a581eee7428d1b
SalvadorCT <salvadorlinc@gmail.com>
2022-09-29 23:04:33 -0500

    Avance listar cadidatos

--- src/controllers/candidatos.controller.js
+++ src/controllers/candidatos.controller.js
@@ -6,11 +6,30 @@ export const crearCandidato = async (req, res) => {
     try{
         const partidoPolitico = await conexion.partidoPolitico.findUniqueOrThrow({
             where: {id: data.partidoPoliticoId},
+            select: {
+                nombre: true,
+            }
         });
-
+        // SELECT nombre FROM partidoPolitico WHERE id = ...
         const region = await conexion.region.findUniqueOrThrow({
             where: {id: data.regionId},
+            select: {
+                nombre: true,
+            }
         });
+        const candidato = await conexion.candidato.findFirst({
+            where: {
+                partidoPoliticoId: data.partidoPoliticoId,
+                regionId: data.regionId,
+            },
+        });
+
+        if (candidato !== null) {
+            throw new Error(
+                `El candidato por el partido ${partidoPolitico.nombre} por la region ${region.nombre} ya existe`
+                );
+        }
+
         const resultado = await conexion.candidato.create({
             data,
         });
@@ -25,8 +44,32 @@ export const crearCandidato = async (req, res) => {
                 content: error.message,
             });
         }
-        return res.status(400).json({
+        return res.status(404).json({
             message: 'Error desconocido',
+            content: error.message,
         })
     }
+};
+
+export const listarCandidatos = async (req, res) => {
+    const {regionId, partidoPoliticoId} = req.query;
+    // vamos a poder buscar a los candidatos mediante la region o partido politico o ambos
+    let where = {};
+
+    if(regionId){
+        console.log('hay regionId');
+        where = {...where,regionId: parseInt(regionId)};
+    };
+    if(partidoPoliticoId){
+        console.log('hay partidoPoliticoId');
+        where = {...where,partidoPoliticoId: +partidoPoliticoId};
+    }
+
+    const candidatos = await conexion.candidato.findMany({
+        where,
+    });
+
+    return res.json({
+        candidatos,
+    });
 };
